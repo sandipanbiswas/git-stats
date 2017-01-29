619d77f331866c8a95d50b1a869024b248837f2d
sandipanbiswas6@gmail.com <sandipan.biswas@onedirect.in>
2017-01-29 21:44:46 +0530

    default

--- com/almabase/github/manager/GitManager.py
+++ com/almabase/github/manager/GitManager.py
@@ -1,6 +1,6 @@
 from com.almabase.github.service.GitHubService import GitHubService
 class GitManager:
-    def topNRepos(self,org,top_repo_count):
+    def topNRepos(self,org,top_repo_count=5):
         topRepos=GitHubService().getTopNReposByOrg(org,top_repo_count)
         if len(topRepos)>0:
             print 'Top '+ str(top_repo_count) + ' repos in order based on fork_count as follows \n'
@@ -8,7 +8,7 @@ class GitManager:
                 print 'Name : '+ topRepo[1]['name'] + ' url :' + topRepo[1]['url'] + ' fork_count ' + str(topRepo[1]['forks_count'])
         return topRepos
 
-    def topNCommiters(self, org, top_repo_count, top_commiter_count):
+    def topNCommiters(self, org, top_repo_count=5, top_commiter_count=3):
         topNRepos=self.topNRepos(org,top_repo_count)
         for repo in topNRepos:
             topCommiters=GitHubService().getTopNCommittersByRepo(repo,top_commiter_count)
@@ -20,9 +20,9 @@ class GitManager:
         print '\n'
 
     def start(self):
-        org = raw_input('Enter name of organisation for which no of top repositories you want to fetch: ')
-        no_of_repos=raw_input('Enter no of top repositories you want to fetch: ')
-        no_of_committers=raw_input('Enter no of top committers in each repo you want to fetch: ')
+        org = raw_input('Enter name of organisation for which no of top repositories you want to fetch: ') or "google"
+        no_of_repos=raw_input('Enter no of top repositories you want to fetch: ') or 5
+        no_of_committers=raw_input('Enter no of top committers in each repo you want to fetch: ') or 3
         self.topNCommiters(org, no_of_repos, no_of_committers)
 
 
